4943f860486dc0d98cf5dffb39324f1f1431bfc8
Brian Bartholomew <me@bcbrian.com>
2016-04-17 15:36:43 +0000

    got it caught back up

--- tests/features/steps/Given_I_am_on_the_poker_site.js
+++ tests/features/steps/Given_I_am_on_the_poker_site.js
@@ -2,4 +2,13 @@ module.exports = function() {
   this.Given(/^I am on the poker site$/, function () {
     browser.url('http://localhost:3000');
   });
+  
+  this.Then(/^I should see "([^"]*)"$/, function (arg1) {
+    // <h1 className="display-3">Some Page!</h1>
+    let _el = 'h1.display-3';
+    browser.waitForExist(_el);
+    expect(browser.getText(_el)).toEqual(arg1);
+    // Write the automation code here
+  });
 };
+
